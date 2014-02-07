b5b6a9d315ae57a7fe39e8869a03a78e77c7f770
Robert Austin <robert@oatlab.com>
2014-02-07 09:43:39 -0500

    Added checkboxes to tasks

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var form = document.forms[0];
 var list = document.getElementById("tasks");
 var handleSubmit = function (event) {
 	event.preventDefault();
+
+	var todoText = form.todo.value;
 	var task = document.createElement("li");
-	task.innerText = form.todo.value;
+
+	task.innerHTML = "<input type=\"checkbox\" /> " + todoText;
+
 	list.insertBefore(task,list.firstChild);
+
 	form.todo.value = "";
 };
 form.addEventListener("submit",handleSubmit,false);
