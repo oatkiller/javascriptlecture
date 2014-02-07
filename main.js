97454a8e86918c51aca7ecee5cd15e0ab3849999
Robert Austin <robert@oatlab.com>
2014-02-07 09:27:49 -0500

    Printing the value of the input when the form is submitted.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var form = document.forms[0];
 var handleSubmit = function (event) {
     event.preventDefault();
+    console.log(form.todo.value);
 };
 form.addEventListener("submit",handleSubmit,false);
