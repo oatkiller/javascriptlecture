fb293c446fa17a2b7cec72a9102ba1fb54a7c21a
Robert Austin <robert@oatlab.com>
2014-02-07 09:34:28 -0500

    Add a list element on submit

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var form = document.forms[0];
+var list = document.getElementById("tasks");
 var handleSubmit = function (event) {
-    event.preventDefault();
-    console.log(form.todo.value);
+	event.preventDefault();
+	var task = document.createElement("li");
+	task.innerText = form.todo.value;
+	list.insertBefore(task,list.firstChild);
 };
 form.addEventListener("submit",handleSubmit,false);
